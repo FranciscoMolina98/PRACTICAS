187d8a1f142b7a33d43bf15adff8f5def67b2983
Francisco Molina <142026097+FranciscoMolina98@users.noreply.github.com>
2023-09-02 00:56:08 -0300

    02/09/23 - 00:56

--- EJERCICIOS/src/EJERCICIO1.c
+++ EJERCICIOS/src/EJERCICIO1.c
@@ -11,9 +11,9 @@ P0,0 al P0.9.
 #ifdef __USE_CMSIS
 #include "LPC17xx.h"
 
-#define APAGADO 0b  // 11 1111 1111
-#define IMPAR 0b155 // 01 0101 0101  
-#define PAR 0b2AA   // 10 1010 1010
+#define APAGADO 0x3FF  		// 11 1111 1111
+#define IMPAR 0x155 		// 01 0101 0101  
+#define PAR 0x2AA   		// 10 1010 1010
 
 void config_GPIO();
 void delay (unsigned int count);
@@ -50,23 +50,23 @@ void config_GPIO(){
 
 void mostrarPar () //La secuencia dura aprox 2 delays
 {
-    int count=20;                    //con este valor puedo modificar los tiempos del delay   
-	LPC_GPIO0 -> FIOCLR |= APAGADO;  //Apagamos todos los led
+    int count=20;                    	//con este valor puedo modificar los tiempos del delay   
+	LPC_GPIO0 -> FIOCLR |= APAGADO; //Apagamos todos los led
     delay(count);
-	LPC_GPIO0 -> FIOSET |= PAR;      //Prendemos la configuracion de led de los puertos pares
+	LPC_GPIO0 -> FIOSET |= PAR;     //Prendemos la configuracion de led de los puertos pares
     delay(count);
 }
 
 void mostrarImpar (const short int secuencia) //La secuencia dura aprox 2 delays
 {
-    int count=20;                   //con este valor puedo modificar los tiempos del delay
+    int count=20;                   	//con este valor puedo modificar los tiempos del delay
 	LPC_GPIO0->FIOCLR |= APAGADO;   //Apagamos todos los leds
 	delay(count);
-    LPC_GPIO0->FIOSET |= IMPAR;     //Prendemos la configuracion impar de leds
+    LPC_GPIO0->FIOSET |= IMPAR;    	//Prendemos la configuracion impar de leds
     delay(count);
 }
 
-void delay (unsigned int count)     //cant*(2 delay)=duracion de cada secuencia
+void delay (unsigned int count)     	//cant*(2 delay)=duracion de cada secuencia
 {
 	for(int i=0;i<count;i++){
 		for(int j=0;j<1000;j++);
