34630058503c3c74ac28a624932ffd5d0e4b2b05
Francisco Molina <francisco.molina98@mi.unc.edu.ar>
2023-09-02 05:20:54 -0300

    02/09/2023 - 05:20

--- EJERCICIOS/src/EJERCICIO1.c
+++ EJERCICIOS/src/EJERCICIO1.c
@@ -15,10 +15,10 @@ P0,0 al P0.9.
 #define IMPAR 0x155 		// 01 0101 0101  
 #define PAR 0x2AA   		// 10 1010 1010
 
-void config_GPIO();
+void config_GPIO(void);
 void delay (unsigned int count);
-void mostrarPar();
-void mostrarImpar();
+void mostrarPar(void);
+void mostrarImpar(void);
 
 
 int main(void) {
@@ -29,11 +29,11 @@ int main(void) {
         int cant=10;        //Si cambio cant puedo cambiar la duracion de la secuencia
 		for(int i=0; i<cant; cont++)
         {
-            mostrarPar();   //secuencia par = secuencia A
+            void mostrarPar();   //secuencia par = secuencia A
 		}
         for(int i=0; i<cant; i++)
         {
-			mostrarImpar(); ////secuencia impar = secuencia B
+			void mostrarImpar(); ////secuencia impar = secuencia B
 		}
 	}
     return 0 ;
@@ -41,7 +41,6 @@ int main(void) {
 
 
 void config_GPIO(){
-
 	LPC_PINCON -> PINSEL0 &= ~(0x3FF<<0);       
     //configuramos los pin <P0.9:P0.0> como GPIO
 	LPC_GPIO0  -> FIODIR |= 0x3FF;  
--- EJERCICIOS/src/EJERCICIO3.c
+++ EJERCICIOS/src/EJERCICIO3.c
@@ -6,9 +6,56 @@ variable "acumulador".El valor inicial de "acumulador" es 0.
 
 #ifdef __USE_CMSIS
 #include "LPC17xx.h"
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// Definición de pines
+#define PIN_P04 (4)
+#define PIN_P00 (0)
+#define PIN_P01 (1)
+#define PIN_P02 (2)
+#define PIN_P03 (3)
+
+// Declaro funciones
+void configurarPuerto(void);
+
+// Variables globales
+int acumulador = 0;
 
 
+int main() {
+    void configurarPuertos();
 
+    while (1) {
+        // Leer el estado del pin P0.4
+        bool valorP04 = ((LPC_GPIO0->FIOPIN>>PIN_P04) & (1 << PIN_P04)) ? true : false;
 
+        // Leer el valor en los pines P0.0 al P0.3
+        int valorLeido = ((LPC_GPIO0->FIOPIN>>PIN_P00) & 0x0F);
 
+        // Realizar la suma o resta basada en el estado de P0.4
+        if (valorP04) //Si p0:4 esta en 1, se suma
+        {
+            acumulador += valorLeido;
+        }
+        else //Si p0:4 esta en 0, se suma
+        {
+            acumulador -= valorLeido;
+        }
+    }
 
+    return 0;
+}
+
+void configurarPuertos() {
+    LPC_PINCON->PINSEL0 &= ~(0x3FF<<0);
+    //P0.4, P0.0 as GPIO  (Es necesario que <P0.3:P0.4> sean declarados como gpio?)
+
+    LPC_PINCON -> PINMODE0 |= (2 << (2 * PIN_P04));
+    // Habilita la resistencia de pull-down en P0.4
+
+    LPC_GPIO0->FIODIR &= ~(1 << PIN_P04);
+    // Configura P0.4 como entrada
+}
